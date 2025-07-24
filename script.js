e99bea61d59c6e74ed410438c622d207d0c5212d
Moisés <olimpiodev.tec@gmail.com>
2025-07-24 08:06:24 -0300

    Recebendo dados

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+let clientWeb = null
+
+const clientId = "Esp32" + Math.floor(Math.random() * 900) + 100
+//clientWeb = new Paho.MQTT.Client("broker.emqx.io", 8084, clientId)
+clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId)
+
+clientWeb.connect({
+    useSSL: true,
+    timeout: 5,
+    onSuccess: function() {
+        alert("Conectado com sucesso :)")
+    },
+    onFailure: function(error) {
+        alert("A conexão falhou :(")
+        console.log(error)
+    }
+})
 
 
 function ligarVermelho() {
     document.getElementById("vermelho").classList.add("vermelho")
+    
+    // Está fazendo o publish no tópico
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai510/semaforo/ligar/vermelho"
+    clientWeb.send(msg)
 }
 
 function ligarAmarelo() {
     document.getElementById("amarelo").classList.add("amarelo")
+
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai510/semaforo/ligar/amarelo"
+    clientWeb.send(msg)
 }
 
 function ligarVerde() {
     document.getElementById("verde").classList.add("verde")
+
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai510/semaforo/ligar/verde"
+    clientWeb.send(msg)
 }
 
 function ligarAutomatico() {
@@ -20,4 +50,8 @@ function desligar() {
     document.getElementById("vermelho").classList.remove("vermelho")
     document.getElementById("amarelo").classList.remove("amarelo")
     document.getElementById("verde").classList.remove("verde")
+
+    const msg = new Paho.MQTT.Message("")
+    msg.destinationName = "senai510/semaforo/desligar"
+    clientWeb.send(msg)
 }
